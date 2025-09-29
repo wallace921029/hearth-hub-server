310afcacc7f54c862dfcfbf0a7bc00e0c9d31a74
Wallace <youzhi.wang.1029@outlook.com>
2025-09-29 17:07:39 +0800

    fix: .

--- src/router/auth/index.ts
+++ src/router/auth/index.ts
@@ -4,38 +4,38 @@ import authService from "../../service/auth/index.js";
 import { fail, success } from "../../utils/common-result/index.js";
 
 function authRoutes(fastify: FastifyInstance) {
-    /** sign up */
-    fastify.post('/sign-up', async (request, reply) => {
-        const body = request.body as SignUpDTO;
-        try {
-            const result = await authService.signUpService(fastify, body)
+  /** sign up */
+  fastify.post("/sign-up", async (request, reply) => {
+    const body = request.body as SignUpDTO;
+    try {
+      const result = await authService.signUpService(fastify, body);
 
-            if (result) {
-                return success(null, 'User registered successfully')
-            }
-        } catch (error: any) {
-            return fail(error.message);
-        }
-    })
+      if (result) {
+        return success(null, "User registered successfully");
+      }
+    } catch (error: any) {
+      return fail(error.message);
+    }
+  });
 
-    /** sign in */
-    fastify.post('/sign-in', async (request, reply) => {
-        const body = request.body as SignInDTO
-        try {
-            const token = await authService.signInService(fastify, body)
+  /** sign in */
+  fastify.post("/sign-in", async (request, reply) => {
+    const body = request.body as SignInDTO;
+    try {
+      const token = await authService.signInService(fastify, body);
 
-            if (token) {
-                return success(token, 'User signed in successfully')
-            }
-        } catch (error: any) {
-            return fail(error.message);
-        }
-    })
+      if (token) {
+        return success(token, "User signed in successfully");
+      }
+    } catch (error: any) {
+      return fail(error.message);
+    }
+  });
 
-    /** sign out */
-    fastify.get('/sign-out', async (request, reply) => {
-        return success(null, 'User signed out successfully')
-    })
+  /** sign out */
+  fastify.get("/sign-out", async (request, reply) => {
+    return success(null, "User signed out successfully");
+  });
 }
 
 export default authRoutes;
